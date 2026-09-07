04a7350a8558dbfda665c74dda63ce0e841956db
tufan8877 <tufandonmezyurek@gmail.com>
2026-09-07 16:40:37 +0200

    Load responsive original-brand asset styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './siegel.css';
+import './brand-fix.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
